8188f9556d336c48f668ab49f53188a6a794b9c5
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 15:47:02 +0200

    Implement rotateRight

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -9,6 +9,16 @@ export class RotatingShape {
         return new RotatingShape(string.trim().split("\n").map(row => row.trim().split("")));
     }
 
+    rotateRight(){
+        const size = this.grid.length;
+        const newGrid = Array.from({ length: size }, () => Array(size));
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                newGrid[i][j] = this.grid[size - j - 1][i];
+            }
+        }
+        return new RotatingShape(newGrid);
+    }
     toString() {
         return this.grid.map(row => row.join("")).join("\n") + "\n";
     }
