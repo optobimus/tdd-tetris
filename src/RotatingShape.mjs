2c26ed73b0e87d5abf839edda36b33aacd405b78
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 15:37:17 +0200

    Implement base object and properties for RotatingShape

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,15 @@
+export class RotatingShape {
+    grid = [];
+
+    constructor(grid) {
+        this.grid = grid;
+    }
+
+    static fromString(string) {
+        return new RotatingShape(string.trim().split("\n").map(row => row.trim().split("")));
+    }
+
+    toString() {
+        return this.grid.map(row => row.join("")).join("\n") + "\n";
+    }
+}
