f49444b4e6c514dca039b1da306a54e29a587109
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 14:46:48 +0200

    Implement 2D position array and initialize drop function

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,22 +1,28 @@
 export class Board {
   width;
   height;
+  positions = [];
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.positions = Array.from({ length: height }, () => Array(width).fill("."));
+  }
+
+  drop(block) {
+     const middleIdx = Math.floor(this.width / 2);
+     this.positions[0][middleIdx] = block;
   }
 
   toString() {
     let result = "";
     for (let row = 0; row < this.height; row++) {
       for (let column = 0; column < this.width; column++) {
-        result += ".";
+        result += this.positions[row][column];
       }
       result += "\n";
     }
     return result;
-
-
   }
+
 }
