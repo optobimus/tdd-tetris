e7b03ca330fefa153d6eb4a5b16cb0c671ceb4bb
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 15:48:06 +0200

    Implement rotateLeft through rotateRight ;)

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -19,6 +19,11 @@ export class RotatingShape {
         }
         return new RotatingShape(newGrid);
     }
+
+    rotateLeft() {
+        return this.rotateRight().rotateRight().rotateRight();
+    }
+    
     toString() {
         return this.grid.map(row => row.join("")).join("\n") + "\n";
     }
