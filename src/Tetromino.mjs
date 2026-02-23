3e9089aa7c1061c29320e0ca76b9441fb459c655
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-23 12:58:25 +0200

    Replace T shape Tetromino with fixed orientations

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -7,6 +7,7 @@ export class Tetromino {
         this.#orientations = orientations.map((o) => typeof o === "string" ? RotatingShape.fromString(o) : o);
         this.#currentIndex = currentIndex;
     }
+
     rotateRight() {
         return new Tetromino(this.#orientations, (this.#currentIndex + 1) % this.#orientations.length);
     }
@@ -17,11 +18,7 @@ export class Tetromino {
         return this.#orientations[this.#currentIndex].toString();
     }
 
-    static T_SHAPE = RotatingShape.fromString(
-        `.T.
-         TTT
-         ...`
-    );
+    static T_SHAPE = new Tetromino([`.T.\nTTT\n...`, `.T.\n.TT\n.T.`, `...\nTTT\n.T.`, `.T.\nTT.\n.T.`]);
 
     static I_SHAPE = RotatingShape.fromString(
         `.....
