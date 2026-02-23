e9738c5206e195f1e142b7885d9996f55a212b62
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-23 15:17:50 +0200

    Implement tests checking that blocks cannot be moved through other blocks. Already pass due to earlier implementation

--- test/MovingFallingTetrominoes.test.mjs
+++ test/MovingFallingTetrominoes.test.mjs
@@ -100,4 +100,63 @@ describe("Moving falling tetrominoes", () => {
     );
     expect(board.hasFalling()).to.be.false;
   });
+
+  test("it cannot be moved left through other blocks", () => {
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+    for (let i = 0; i < 10; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
+    for (let i = 0; i < 10; i++) board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ...T......
+       .TTTT.....
+       TTT.......`
+    );
+  });
+
+  test("it cannot be moved right through other blocks", () => {
+    for (let i = 0; i < 10; i++) board.moveRight();
+    for (let i = 0; i < 10; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
+    for (let i = 0; i < 10; i++) board.moveRight();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ......T...
+       .....TTTT.
+       .......TTT`
+    );
+  });
+
+  test("it cannot be moved down through other blocks (will stop falling)", () => {
+    for (let i = 0; i < 10; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 10; i++) board.moveDown();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ....T.....
+       ...TTT....
+       ....T.....
+       ...TTT....`
+    );
+    expect(board.hasFalling()).to.be.false;
+  });
 });
