22a92817459b207ada6601d9acaa14228cd4940d
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-23 13:53:00 +0200

    Create class Block

---
+++ src/Block.mjs
@@ -0,0 +1,12 @@
+export class Block {
+  #char;
+  constructor(char) {
+    this.#char = char;
+  }
+
+  get width() { return 1; }
+  get height() { return 1; }
+  
+  cellAt(row, col) { return this.#char; }
+  toString() { return this.#char + "\n"; }
+}
