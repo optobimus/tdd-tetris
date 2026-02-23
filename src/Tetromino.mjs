9791e08b1466c5bfbcd536e5d0be7fc8621b5548
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-23 12:55:40 +0200

    Lay the groundwork for a Tetromino refactor, defining fixed orientations in the class

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,6 +1,13 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
+    #orientations;
+    #currentIndex;
+    constructor(orientations, currentIndex = 0) {
+        this.#orientations = orientations.map((o) => typeof o === "string" ? RotatingShape.fromString(o) : o);
+        this.#currentIndex = currentIndex;
+    }
+    
     static T_SHAPE = RotatingShape.fromString(
         `.T.
          TTT
