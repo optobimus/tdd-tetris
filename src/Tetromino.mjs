2abce571c2c1dc1f0c1041107b944c37f1022c3a
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-23 12:56:40 +0200

    Implement new custom rotate methods for Tetrominos. They are not called yet

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -7,7 +7,16 @@ export class Tetromino {
         this.#orientations = orientations.map((o) => typeof o === "string" ? RotatingShape.fromString(o) : o);
         this.#currentIndex = currentIndex;
     }
-    
+    rotateRight() {
+        return new Tetromino(this.#orientations, (this.#currentIndex + 1) % this.#orientations.length);
+    }
+    rotateLeft() {
+        return new Tetromino(this.#orientations, (this.#currentIndex - 1 + this.#orientations.length) % this.#orientations.length);
+    }
+    toString() {
+        return this.#orientations[this.#currentIndex].toString();
+    }
+
     static T_SHAPE = RotatingShape.fromString(
         `.T.
          TTT
