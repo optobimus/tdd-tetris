0248ac81880e0bba82cf06a10f80278c1f23d136
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-24 13:48:11 +0200

    Improve clearing lines test cases and add test for shifting down after clearing

--- test/ClearingLines.test.mjs
+++ test/ClearingLines.test.mjs
@@ -5,29 +5,45 @@ import { Board } from "../src/Board.mjs";
 describe("Clearing lines", () => {
   let board;
   beforeEach(() => {
-    board = new Board(3, 3);
+    board = new Board(3, 4);
   });
 
   test("a full row is cleared when a block lands", () => {
     board.drop("X");
     board.moveLeft();
-    board.tick();
-    board.tick();
-    board.tick();
+    board.tick(); board.tick(); board.tick(); board.tick();
     board.drop("X");
-    board.tick();
-    board.tick();
-    board.tick();
+    board.tick(); board.tick(); board.tick(); board.tick();
     board.drop("X");
     board.moveRight();
-    board.tick();
-    board.tick();
-    board.tick();
+    board.tick(); board.tick(); board.tick(); board.tick();
 
     expect(board.toString()).to.equalShape(
       `...
        ...
+       ...
        ...`
+    );
+  });
+
+  test("rows above a cleared line shift down", () => {
+    board.drop("X");
+    board.moveLeft();
+    board.tick(); board.tick(); board.tick(); board.tick();
+    board.drop("X");
+    board.moveLeft();
+    board.tick(); board.tick(); board.tick();
+    board.drop("X");
+    board.tick(); board.tick(); board.tick(); board.tick();
+    board.drop("X");
+    board.moveRight();
+    board.tick(); board.tick(); board.tick(); board.tick();
+
+    expect(board.toString()).to.equalShape(
+      `...
+       ...
+       ...
+       X..`
     );
   });
 });
